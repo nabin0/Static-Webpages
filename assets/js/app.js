de65beb3fdf6045e7026faf9af7e4901526a7e07
Nabin <nabinbhatt62@gmail.com>
2021-05-18 14:30:24 +0530

    Dev Process

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,14 @@ var themeBox = document.getElementById("theme");
 themeMode.addEventListener("click", (e) => {
   if (document.documentElement.hasAttribute("theme")) {
     document.documentElement.removeAttribute("theme");
+    themeMode.innerHTML = ` <i class="fa fa-moon-o" id="themeMode">Dark</i>`;
   } else {
     document.documentElement.setAttribute("theme", "dark");
+    themeMode.innerHTML = ` <i class="fa fa-sun-o" id="themeMode">Light</i>`;
   }
 });
 
 // Theme Box Controller
-
 function ctrlTheme() {
   if (themeCtrl.classList.contains("fa-angle-down")) {
     themeBox.style.right = "-60px";
